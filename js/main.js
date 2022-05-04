4421e28986c1458958bc50a174ac61d45f9b74d4
Jaime <jaime.joseph1991@gmail.com>
2022-05-03 22:36:11 -0400

    added all winning conditions

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ class Game {
     display(event){
         if(event.target.innerHTML == "X" || event.target.innerHTML == "O"){
             return
-        }
+        } // if the box already has X or O in it, you cannot override the box's value with another click
       this.counter % 2 == 0 ? this.text = 'X' : this.text = 'O'
       event.target.innerHTML = this.text
       event.value = this.text
@@ -37,13 +37,39 @@ class Game {
     determineWinner(){
         if (this.list[0].innerHTML == "X" && this.list[1].innerHTML == "X" && this.list[2].innerHTML == "X"){
             console.log('player 1 wins')
-        } else if {
-            return
+        } else if(this.list[0].innerHTML == "O" && this.list[1].innerHTML == "O" && this.list[2].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[0].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[8].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[0].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[8].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[0].innerHTML == "X" && this.list[3].innerHTML == "X" && this.list[6].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[0].innerHTML == "O" && this.list[3].innerHTML == "O" && this.list[6].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[1].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[7].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[1].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[7].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[2].innerHTML == "X" && this.list[5].innerHTML == "X" && this.list[8].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[2].innerHTML == "O" && this.list[5].innerHTML == "O" && this.list[8].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[3].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[5].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[3].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[5].innerHTML == "O"){
+            console.log('player 2 wins')
+        } else if(this.list[6].innerHTML == "X" && this.list[7].innerHTML == "X" && this.list[8].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if(this.list[6].innerHTML == "O" && this.list[7].innerHTML == "O" && this.list[8].innerHTML == "O"){
+            console.log('player 2 wins')
         }
+        return
        
-          
        
-       
+    }
+    displayWinner(){
+        //a modal will pop up and declare winner, it will include a function that resets the game
     }
     reset(){
 
