2b64e7a81e965fff8a3f7ff58d3fa20f67310016
Jaime <jaime.joseph1991@gmail.com>
2022-05-03 21:19:47 -0400

    added more functionality in javascript, user cannot choose the same box twice

--- js/main.js
+++ js/main.js
@@ -11,25 +11,39 @@ box.forEach(box => {
 
 
 
-
 class Game {
-    constructor(text,/*idk what goes in this*/){
+    constructor(player1,player2){
         //this whatever whatever
-        this.text = text
-     this.counter = 0
+        this.player1 = player1
+        this.player2 =  player2
+        this.text = ''
+        this.counter = 0 //private variable that keeps track of what player's turn it is
+        this.list = Array.from(box)
     }
     display(event){
-      //private variable that keeps track of what player's turn it is
+        if(event.target.innerHTML == "X" || event.target.innerHTML == "O"){
+            return
+        }
       this.counter % 2 == 0 ? this.text = 'X' : this.text = 'O'
-      box.innerHTML += this.text
-     event.target.innerHTML = this.text
-     event.value = this.text
-     this.counter++
+      event.target.innerHTML = this.text
+      event.value = this.text
+      box.value = this.text
+      this.counter++
+        
       console.log(this.counter)
-      console.log(event.value)
+      console.log(event.target.innerHTML)
+      this.determineWinner()
     }
     determineWinner(){
-
+        if (this.list[0].innerHTML == "X" && this.list[1].innerHTML == "X" && this.list[2].innerHTML == "X"){
+            console.log('player 1 wins')
+        } else if {
+            return
+        }
+       
+          
+       
+       
     }
     reset(){
 
