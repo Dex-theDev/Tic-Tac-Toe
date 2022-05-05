5478b77129a03fb6f54ceac5a6fdd216623a7c51
Jaime <jaime.joseph1991@gmail.com>
2022-05-05 14:00:00 -0400

    created modal to display winner

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@ box.forEach(box => {
         game.display(event)
     })
 })
-
+const button = document.querySelector('.reset-button')
+button.addEventListener('click', function(){
+    game.reset()
+})
 
 
 class Game {
@@ -19,6 +22,8 @@ class Game {
         this.text = ''
         this.counter = 0 //private variable that keeps track of what player's turn it is
         this.list = Array.from(box)
+        this.isWinner = ''
+        
     }
     display(event){
         if(event.target.innerHTML == "X" || event.target.innerHTML == "O"){
@@ -35,44 +40,75 @@ class Game {
       this.determineWinner()
     }
     determineWinner(){
+        
         if (this.list[0].innerHTML == "X" && this.list[1].innerHTML == "X" && this.list[2].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
+            
         } else if(this.list[0].innerHTML == "O" && this.list[1].innerHTML == "O" && this.list[2].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[0].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[8].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[0].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[8].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[0].innerHTML == "X" && this.list[3].innerHTML == "X" && this.list[6].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[0].innerHTML == "O" && this.list[3].innerHTML == "O" && this.list[6].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[1].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[7].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[1].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[7].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[2].innerHTML == "X" && this.list[5].innerHTML == "X" && this.list[8].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[2].innerHTML == "O" && this.list[5].innerHTML == "O" && this.list[8].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
+        } else if(this.list[2].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[6].innerHTML == "X"){
+            console.log('player 1 wins')
+            this.isWinner = this.player1
+        } else if(this.list[2].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[6].innerHTML == "O"){
+            console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[3].innerHTML == "X" && this.list[4].innerHTML == "X" && this.list[5].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[3].innerHTML == "O" && this.list[4].innerHTML == "O" && this.list[5].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2
         } else if(this.list[6].innerHTML == "X" && this.list[7].innerHTML == "X" && this.list[8].innerHTML == "X"){
             console.log('player 1 wins')
+            this.isWinner = this.player1
         } else if(this.list[6].innerHTML == "O" && this.list[7].innerHTML == "O" && this.list[8].innerHTML == "O"){
             console.log('player 2 wins')
+            this.isWinner = this.player2 
         }
+        this.displayWinner()
         return
        
        
     }
     displayWinner(){
-        //a modal will pop up and declare winner, it will include a function that resets the game
+        //a modal will pop up and declare winner, it will include a function call that resets the game
+        if(!this.isWinner){
+            return
+        }
+        const modal = document.querySelector('.modal')
+        document.querySelector('.modal-text').textContent = `${this.isWinner} Wins!`
+        modal.style.display = 'block'
+        
     }
     reset(){
-
+        document.querySelector('.modal').style.display = 'none'
+        document.querySelectorAll('.box').innerHTML = ''
     }
 }
-let game = new Game
+let game = new Game('player 1','player 2')
